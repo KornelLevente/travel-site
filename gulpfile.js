c26b3faf29ef426ed248397e78ab8e81f2b890b9
Kornel <leventekornel@gmail.com>
2018-04-18 23:01:43 -0400

    Added browsersync. Moved gulp watch and styles files to new js folders in tasks folder. Simplified gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,8 @@
-var gulp = require('gulp');
-var watch = require('gulp-watch');
-var postcss = require('gulp-postcss');
-var autoprefixer = require('autoprefixer');
-var cssvars = require('postcss-simple-vars');
-var nested = require('postcss-nested');
-var cssImport = require('postcss-import');
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
 
 
-gulp.task('default', function() {
-	console.log('Congratulation! You created a gulp tasks.');
-});
 
-gulp.task('html', function() {
-	console.log('A change has been made to the html file.');
-});
 
-gulp.task('styles', function() {
-	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
-		.pipe(gulp.dest('./app/temp/styles'));
-});
 
-gulp.task('watch', function() {
-	
-	watch('./app/index.html', function() {
-		gulp.start('html');
-	});
-	
-	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('styles');
-	});
-	
-});
+
